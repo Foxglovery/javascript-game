ddec1bb94cdb36c887fa90aecfd6b977a2494922
Foxglovery <foxglovery@gmail.com>
2024-03-05 17:01:51 -0600

    Arrow Controls added

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { Player } from './player.js'
+import { InputHandler } from './input.js';
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
@@ -10,10 +11,11 @@ window.addEventListener('load', function(){
             this.width = width;
             this.height = height;
             this.player = new Player(this);
+            this.input = new InputHandler();
         }
         update(){
             //to test
-            this.player.update();
+            this.player.update(this.input.keys);
         }
         draw(context){
             this.player.draw(context);
--- player.js
+++ player.js
@@ -1,18 +1,28 @@
 //this will draw and construct our character
 export class Player {
-constructor(game){
+  constructor(game) {
     this.game = game;
     this.width = 100;
     this.height = 91.3;
     this.x = 0;
     this.y = this.game.height - this.height;
-    this.image = document.getElementById('player');
-}
-update(){
-// this.x++
-}
-draw(context){
-    
-    context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height)
-}
+    this.image = document.getElementById("player");
+  }
+  update(input) {
+    if (input.includes("ArrowRight")) this.x++;
+    else if (input.includes('ArrowLeft')) this.x--
+  }
+  draw(context) {
+    context.drawImage(
+      this.image,
+      0,
+      0,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
 }
