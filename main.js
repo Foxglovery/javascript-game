fb6970f3228e790c4d36c4ab3d6395e0ae8e8fd6
Foxglovery <foxglovery@gmail.com>
2024-03-05 15:37:45 -0600

    begin making input.js

--- main.js
+++ main.js
@@ -12,13 +12,22 @@ window.addEventListener('load', function(){
             this.player = new Player(this);
         }
         update(){
-
+            //to test
+            this.player.update();
         }
         draw(context){
-            this.player.draw();
+            this.player.draw(context);
         }
     }
 
     const game = new Game(canvas.width, canvas.height);
     console.log(game);
+
+    function animate (){
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        game.update();
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
 });
