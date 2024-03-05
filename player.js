8921369682e6d4937dc450eedc729f3c0c91ba51
Foxglovery <foxglovery@gmail.com>
2024-03-05 15:12:53 -0600

    added player

--- player.js
+++ player.js
@@ -1,6 +1,16 @@
 //this will draw and construct our character
-class Player {
+export class Player {
 constructor(game){
     this.game = game;
+    this.width = 100;
+    this.height = 91.3;
+    this.x = 0;
+    this.y = 100;
+}
+update(){
+
+}
+draw(context){
+    context.fillRect(this.x, this.y, this.width, this.height)
 }
 }
